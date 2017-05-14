e5e44ebf0e670b664f1c2dccb1c6caaa18ce6062
kelly-keating <keating.kel@gmail.com>
2017-05-14 22:50:26 +1200

    fixed seed data

--- seeds/topics.js
+++ seeds/topics.js
@@ -2,9 +2,9 @@ exports.seed = function (knex, Promise) {
   return knex('topics').del()
     .then(function () {
       return Promise.all([
-        {id:1, name:'Command Line', description:'i am neo'},
-        {id:2, name:'git', description:'the shiz'},
-        {id:3, name:'Heroku', decription:'tis I'}
+        knex('topics').insert({id:1, name:'Command Line', description:'i am neo'}),
+        knex('topics').insert({id:2, name:'git', description:'the shiz'}),
+        knex('topics').insert({id:3, name:'Heroku', description:'tis I'})
       ]);
     });
 };
